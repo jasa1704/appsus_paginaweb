93c4094d04c1bbf8670fa519b4c4d833b9f92446
jasa1704 <jasosapanta@gmail.com>
2018-10-02 21:26:24 -0500

    Modificacion video promocional de la empresa

--- enviar2.php
+++ enviar2.php
@@ -5,12 +5,11 @@ $correo = $_POST['correo'];
 
 // Datos para el correo
 $destinatario = "comercial@appsus.co";
-$asunto = "Mensaje envio desde la pagina principal";
+$asunto = "Mensaje envio desde la pagina principal de APPSUS.CO";
 
-$carta = "De: Usuario pagina principal";
-$carta .= "Correo: $correo \n";
-$carta .= "Telefono: No se tiene";
-$carta .= "Mensaje: Quisiera saber mas sobre mi alcaldia";
+$carta = "De:Posible cliente ¡URGENTE!\n";
+$carta .= "Correo: $correo\n";
+$carta .= "Mensaje: Quisiera saber mas sobre mi alcaldia, por favor contactarse conmigo.";
 
 // Enviando Mensaje
 
